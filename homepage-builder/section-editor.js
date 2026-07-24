00d7471e16add7c4da9e175824aa4dafc27bd1df
Imaginary Gifts <imaginarygifts.in@gmail.com>
2026-07-24 18:33:59 +0530

    Update section-editor.js

--- homepage-builder/section-editor.js
+++ homepage-builder/section-editor.js
@@ -1,6 +1,14 @@
 /*==================================================
     SECTION EDITOR
 ==================================================*/
+import { storage } from "../js/firebase.js";
+
+import {
+    ref,
+    uploadBytes,
+    getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
 
 export function renderSectionEditor({
     container,
